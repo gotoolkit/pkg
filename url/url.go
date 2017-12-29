84611edadec54e632c0cf5432e26c667f5d1e415
llitfkitfk <llitfkitfk@gmail.com>
2017-12-29 14:18:18 +0800

    add url

---
+++ url/url.go
@@ -0,0 +1,36 @@
+package url
+
+import (
+	"regexp"
+	"strings"
+)
+
+var (
+	validPrefixes = map[string][]string{
+		"url": {"http://", "https://"},
+		"git": {"git://", "github.com/", "git@"},
+	}
+	urlPathWithSuffix = regexp.MustCompile(".git(?:#.+)?$")
+)
+
+// IsURL returns true if the provided str is an HTTP(s) URL
+func IsURL(str string) bool {
+	return checkURL(str, "url")
+}
+
+// IsGitURL returns true if the provided str is a git repository URL
+func IsGitURL(str string) bool {
+	if IsURL(str) && urlPathWithSuffix.MatchString(str) {
+		return true
+	}
+	return checkURL(str, "git")
+}
+
+func checkURL(str, kind string) bool {
+	for _, prefix := range validPrefixes[kind] {
+		if strings.HasPrefix(str, prefix) {
+			return true
+		}
+	}
+	return false
+}
---
+++ url/url_test.go
@@ -0,0 +1,35 @@
+package url
+
+import "testing"
+
+var (
+	gitUrls = []string{
+		"git://github.com/gotoolkit/pkg",
+		"https://github.com/gotoolkit/pkg.git",
+		"http://github.com/gotoolkit/pkg.git",
+	}
+	incompleteGitUrls = []string{
+		"github.com/gotoolkit/pkg",
+	}
+	invalidGitUrls = []string{
+		"http://github.com/gotoolkit/pkg.git:#branch",
+	}
+)
+
+func TestIsGitURL(t *testing.T) {
+	for _, url := range gitUrls {
+		if !IsGitURL(url) {
+			t.Fatalf("%q should be detected as valid Git url", url)
+		}
+	}
+	for _, url := range incompleteGitUrls {
+		if !IsGitURL(url) {
+			t.Fatalf("%q should be detected as valid Git url", url)
+		}
+	}
+	for _, url := range invalidGitUrls {
+		if IsGitURL(url) {
+			t.Fatalf("%q should be detected as valid Git prefix", url)
+		}
+	}
+}
